1f1d92cb19f6cab165c0973602587dc495c6a68b
dangtung <language0512@gmail.com>
2023-05-05 19:31:41 +0700

    add middleware

--- src/app.js
+++ src/app.js
@@ -1,4 +1,13 @@
 const app = require("express")();
+const morgan = require("morgan");
+const helmet = require("helmet");
+const compression = require("compression");
+
+
+//init middleware
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression());
 
 const server = app.listen(3000, () => {
     console.log("app listen on port 3000")
