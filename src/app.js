3de5d44d9ad3160b173392c17e0585b25be5bdae
dangtung <language0512@gmail.com>
2023-05-05 19:12:26 +0700

    update app

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 const app = require("express")();
 
-app.listen(3000, () => {
+const server = app.listen(3000, () => {
     console.log("app listen on port 3000")
+})
+
+process.on("SIGINT", () => {
+    server.close(() => {
+        console.log("Server Exited")
+    })
 })
