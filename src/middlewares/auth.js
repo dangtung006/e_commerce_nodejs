08d1eafdb359cde32f664af266975cac0e1ff989
dangtung <language0512@gmail.com>
2023-05-06 18:18:43 +0700

    update routes middlewares

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -3,6 +3,8 @@ const HEADER = {
     AUTHORIZATION: 'authorization'
 }
 
+const ApiKeyRepository = require("../repositories/api_key");
+
 const apiKey = async (req, res, next) => {
     try {
         const key = req.headers[HEADER['API_KEY']]?.toString();
@@ -10,8 +12,31 @@ const apiKey = async (req, res, next) => {
             message: "Forbiden Error"
         });
 
+        const apiKeyObj = await ApiKeyRepository.getOneByConditions({ key });
+        if (!apiKeyObj) return res.status(403).json({
+            message: "Forbiden Error"
+        });
+        req.apiKeyObj = apiKeyObj;
         return next();
     } catch (err) {
         console.error(err)
     }
+}
+
+const permission = (permission) => {
+    return (req, res, next) => {
+        if (!req.apiKeyObj.permissions) return res.status(403).json({
+            message: "Permission Debied"
+        });
+
+        if (!req.apiKeyObj.permissions.includes(permission)) return res.status(403).json({
+            message: "Permission Debied"
+        });
+
+        return next();
+    }
+}
+module.exports = {
+    apiKey,
+    permission
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const AuthRoutes = require("./modules/auth.js");
 const router = express.Router();
-
+const { apiKey, permission } = require("../middlewares/auth.js")
 const routeConfigs = [
     {
         "routeName": "/auth",
@@ -10,6 +10,10 @@ const routeConfigs = [
 ]
 
 const AppRoutes = () => {
+
+    router.use(apiKey);
+    router.use(permission("0000"));
+
     for (let idx = 0; idx < routeConfigs.length; idx++) {
         if (routeConfigs[idx].routeName && routeConfigs[idx].router) {
             router.use(routeConfigs[idx].routeName, routeConfigs[idx].router)
