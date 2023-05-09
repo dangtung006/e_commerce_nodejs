a09e8879f990f7a7717b1d7c7d570b9e167cffb9
dangtung <language0512@gmail.com>
2023-05-09 12:39:20 +0700

    update base repository

--- src/repositories/base.js
+++ src/repositories/base.js
@@ -1,28 +1,48 @@
 class BaseRepository {
     _Entity = null;
+    select = {};
+    join = {};
+    session = {};
+    sort = {};
+    paging = { offset: 0, limit: 10 };
+
+    defaultOpt = {
+        offset: this.paging["offset"],
+        limit: this.paging["limit"],
+        sort: this.sort
+    }
 
     constructor(opt) {
-        this._Entity = opt && opt.entity ? opt.entity : null
+        this._Entity = opt && opt.entity ? opt.entity : null;
     }
 
     getById(id) {
-        return this._Entity.findById(id)
+        return this._Entity.findById(id);
     }
 
-    getAll() {
-        return this._Entity.find()
+    getAll(opt = this.defaultOpt) {
+        var sort = opt && opt.sort ? opt.sort : this.sort;
+        return this._Entity.find().sort(sort).limit(10000);
     }
 
-    getList() {
-        return this._Entity.find().skip().limit()
+    getList(opt = this.defaultOpt) {
+        var offset = opt && opt.offset ? opt.offset : this.paging['offset'];
+        var limit = opt && opt.limit ? opt.limit : this.paging['limit'];
+        var sort = opt && opt.sort ? opt.sort : this.sort;
+
+        return this._Entity.find().skip(offset).sort(sort).limit(limit);
     }
 
     getOneByConditions(condition) {
         return this._Entity.findOne(condition)
     }
 
-    getListByConditions(condition) {
-        return this._Entity.findOne(condition).skip().limit();
+    getListByConditions(condition, opt = this.defaultOpt) {
+        var offset = opt && opt.offset ? opt.offset : this.paging['offset'];
+        var limit = opt && opt.limit ? opt.limit : this.paging['limit'];
+        var sort = opt && opt.sort ? opt.sort : this.sort;
+
+        return this._Entity.find(condition).skip(offset).limit(limit).sort(sort);
     }
 
     create(opt) {
