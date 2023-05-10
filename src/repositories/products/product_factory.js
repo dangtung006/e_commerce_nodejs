ef20c5cfff78e1564a93ecff27c5ab476b134aa9
dangtung <language0512@gmail.com>
2023-05-10 18:34:54 +0700

    update product factory repository

--- src/repositories/products/product_factory.js
+++ src/repositories/products/product_factory.js
@@ -3,10 +3,15 @@ const ElectronicsRepository = require("./electrics");
 
 class ProductFactory {
 
+    static productRepositories = {}
+    static initProductRepositories(types, classRef) {
+        this.productRepositories[types] = classRef
+    }
     static async createProduct(productType, payload) {
-        if (productType == "Clothes") return new ClothesRepository(payload).createProduct()
-        if (productType == "Electronics") return new ElectronicsRepository(payload).createProduct();
+        return new this.productRepositories[productType](payload).createProduct()
     }
 };
 
+ProductFactory.initProductRepositories("Clothes", ClothesRepository);
+ProductFactory.initProductRepositories("Electronics", ElectronicsRepository);
 module.exports = ProductFactory;
