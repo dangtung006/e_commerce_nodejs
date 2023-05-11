6e8c14f86c16aa4080cb2b64adc2caa2124b4501
dangtung <language0512@gmail.com>
2023-05-11 14:11:26 +0700

    update

--- src/commons/handle_data/handle_object.js
+++ src/commons/handle_data/handle_object.js
@@ -11,9 +11,34 @@ const getSelectData = (select = []) => {
 const getUnselectData = (unSelect = []) => {
     return Object.fromEntries(unSelect.map(el => [el, 0]));
 }
+const removeUndefinedObj = (obj) => {
+    Object.keys(obj).forEach(k => {
+        if (!obj[k]) {
+            delete obj[k]
+        }
+    });
+    return obj;
+}
+
+const updateNestedObj = (Obj) => {
+    const result = {};
+    Object.keys(Obj).forEach(k => {
+        if (typeof Obj[k] === 'object' && !Array.isArray(Obj[k])) {
+            const res = updateNestedObj(Obj[k]);
+            Object.keys(res).forEach(a => {
+                result[`${k}.${a}`] = res[a]
+            })
+        } else {
+            result[k] = Obj[k]
+        }
+    })
+    return result;
+}
 
 module.exports = {
     pickObjByKey,
     getSelectData,
-    getUnselectData
+    getUnselectData,
+    removeUndefinedObj,
+    updateNestedObj
 }
